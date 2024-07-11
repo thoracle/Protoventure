20fcb0035468443355a9f7fbaa7e180fd20c3c67
Thor Alexander <thoracle@users.noreply.github.com>
2024-07-11 13:05:13 -0700

    Update player.py
    added inventory uc

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
-import random
 from dragon import Dragon
+from item import Item
 
 class Player:
     def __init__(self, name, character_class):
@@ -13,6 +13,7 @@ class Player:
         self.dexterity = 10
         self.intelligence = 10
         self.inventory = []
+        self.max_inventory_size = 10
         self.dragon = None
         self.attack = 10
         self.defense = 5
@@ -29,7 +30,26 @@ class Player:
         return self.health <= 0
 
     def attack_enemy(self):
-        return random.randint(1, self.attack)
+        return self.attack
+
+    def add_item(self, item):
+        if len(self.inventory) < self.max_inventory_size:
+            self.inventory.append(item)
+            return True
+        return False
+
+    def remove_item(self, item_name):
+        for item in self.inventory:
+            if item.name == item_name:
+                self.inventory.remove(item)
+                return item
+        return None
+
+    def use_item(self, item_name):
+        item = self.remove_item(item_name)
+        if item:
+            return item.use(self)
+        return "You don't have that item."
 
     def to_dict(self):
         return {
@@ -42,7 +62,7 @@ class Player:
             "strength": self.strength,
             "dexterity": self.dexterity,
             "intelligence": self.intelligence,
-            "inventory": self.inventory,
+            "inventory": [item.to_dict() for item in self.inventory],
             "dragon": self.dragon.to_dict() if self.dragon else None,
             "attack": self.attack,
             "defense": self.defense
@@ -58,7 +78,7 @@ class Player:
         player.strength = data['strength']
         player.dexterity = data['dexterity']
         player.intelligence = data['intelligence']
-        player.inventory = data['inventory']
+        player.inventory = [Item.from_dict(item_data) for item_data in data['inventory']]
         player.attack = data['attack']
         player.defense = data['defense']
         if data['dragon']:
