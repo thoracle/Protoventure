e42cc7bcd1e861484b2353615f45abbeed7006eb
Thor Alexander <thoracle@users.noreply.github.com>
2024-07-10 22:29:28 -0700

    Update dragon.py
    Added dragon riding use case

--- dragon.py
+++ dragon.py
@@ -8,6 +8,23 @@ class Dragon:
         self.energy = 100
         self.bond_level = 0
         self.abilities = []
+        self.is_ridden = False
+
+    def mount(self):
+        if self.energy >= 10:
+            self.is_ridden = True
+            self.energy -= 10
+            return True
+        return False
+
+    def dismount(self):
+        self.is_ridden = False
+
+    def fly_to(self, location):
+        if self.is_ridden and self.energy >= 20:
+            self.energy -= 20
+            return True
+        return False
 
     def to_dict(self):
         return {
@@ -18,7 +35,8 @@ class Dragon:
             "health": self.health,
             "energy": self.energy,
             "bond_level": self.bond_level,
-            "abilities": self.abilities
+            "abilities": self.abilities,
+            "is_ridden": self.is_ridden
         }
 
     @classmethod
@@ -29,4 +47,5 @@ class Dragon:
         dragon.energy = data['energy']
         dragon.bond_level = data['bond_level']
         dragon.abilities = data['abilities']
+        dragon.is_ridden = data['is_ridden']
         return dragon
